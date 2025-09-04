3933a3103653612d5ef67296a2b55540742f9df5
PedroStabach <pedroaugustostabach3@gmail.com>
2025-09-04 20:14:31 -0300

    criando encontrar pessoas

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,5 +1,22 @@
+import animaisData from './animais.json';
+
 class AbrigoAnimais {
+
+  constructor() {
+    //trazendo os animais do json
+    this.animais = animaisData; //loco nao se importa
+    this.brinquedosValidos = ["RATO", "BOLA", "LASER", "CAIXA", "NOVELO", "SKATE"];
+  }
+
+
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+
+    //separando por virgula
+    const pessoa1 = brinquedosPessoa1.split(',').map((e) => e.trim().toUpperCase());
+    const pessoa2 = brinquedosPessoa2.split(',').map((e) => e.trim().toUpperCase());
+    
+    const listaAnimais = this.ordemAnimais.split(',').map((a) => a.trim());
+
   }
 }
 
